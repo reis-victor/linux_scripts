46a9d61e52af72420a7e9179c455dce978480514
reis-victor <57648039+reis-victor@users.noreply.github.com>
2019-12-26 00:13:47 -0300

    [FIX] Comment formatting

--- 2k_retina.sh
+++ 2k_retina.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-#A Xorg/Gnome oriented bash script that creates a virtual resolution of 4k scaled to 2k in order to simulate a 'retina' display for 2k displays
+# A Xorg/Gnome oriented bash script that creates a virtual resolution of 4k scaled to 2k in order to simulate a 'retina' display for 2k displays
 
 
-#Creates the needed files: resolution_fix.sh and fix_resolution.desktop
+# Creates the needed files: resolution_fix.sh and fix_resolution.desktop
 touch $HOME/resolution_fix.sh $HOME/.config/autostart/fix_resolution.desktop
 
-#Finds the primary output and assigns it to the connected_display variable
+# Finds the primary output and assigns it to the connected_display variable
 connected_display=$(xrandr --verbose 2>/dev/null | grep connected | grep -v disconnected | cut -d' ' -f1)
 
 cat <<EOF > $HOME/resolution_fix.sh
-#2x UI(User Interface) scaling
+# 2x UI(User Interface) scaling
 gsettings set org.gnome.desktop.interface scaling-factor 2
 gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "[{'Gdk/WindowScalingFactor', <2>}]"
-#4k virtual resolution scaled to 2k.
+# 4k virtual resolution scaled to 2k.
 xrandr --output $connected_display --panning 3840x2160 --scale 1.5x1.5
 EOF
 
