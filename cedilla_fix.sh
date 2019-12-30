7242832f4ee5f0daff13fba80884fbb60989d03e
Victor Reis <57648039+reis-victor@users.noreply.github.com>
2019-12-30 12:13:16 -0300

    [Update] Removed [PT-BR] translation for better readability

--- cedilla_fix.sh
+++ cedilla_fix.sh
@@ -1,16 +1,11 @@
 #! /bin/bash
 
-# [EN] A simple script that selects the US-International keyboard layout and enables the cedilla using the ' + c keys combination
-# [PT-BR] Um simples script que seleciona o layout de teclado US-International e habilita a cedilha usando a combinação de teclas: ' + c
+# A simple script that selects the US-International keyboard layout and enables the cedilla using the ' + c keys combination
+# This script should be executed with root privileges
 
-# [EN] This script should be executed with root privileges
-# [PT-BR] Este script deve ser executado com privilégios de root
-
-# [EN] Loads US-International keyboard layout
-# [PT-BR] Carrega o layout de teclado US-International
+# Loads US-International keyboard layout
 loadkeys us-acentos
 
-# [EN] Enables cedilha for GTK and QT applications
-# [PT-BR] Habilita a cedilha para aplicativos GTK e QT
+# Enables cedilha for GTK and QT applications
 echo 'GTK_IM_MODULE="cedilla"
 QT_IM_MODULE="cedilla"' >> /etc/environment
