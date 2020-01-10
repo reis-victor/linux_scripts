38f74f3c3081868886f0688c0f67c7e96f43f005
Victor Reis <57648039+reis-victor@users.noreply.github.com>
2020-01-10 20:35:59 -0300

    [Update] Code block formatting

--- 2k_retina.sh
+++ 2k_retina.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 
-# A Xorg/Gnome oriented bash script that creates a virtual resolution of 4k scaled to 2k in order to simulate a 'retina' display for 2k displays
+# A Xorg/Gnome-oriented bash script that creates a virtual resolution of 4k scaled to 2k in order to simulate a 'retina' display for 2k displays
+
 
 
 # Creates the needed files: resolution_fix.sh and fix_resolution.desktop
 touch $HOME/resolution_fix.sh $HOME/.config/autostart/fix_resolution.desktop
 
+
 # Finds the primary output and assigns it to the connected_display variable
 connected_display=$(xrandr --verbose 2>/dev/null | grep connected | grep -v disconnected | cut -d' ' -f1)
+
 
 cat <<EOF > $HOME/resolution_fix.sh
 # 2x UI(User Interface) scaling
@@ -20,6 +23,7 @@ EOF
 
 # Gives execute permission to all users
 chmod +x $HOME/resolution_fix.sh
+
 
 # Creates an autostart script for the resolution adjustment
 cat <<EOF > $HOME/.config/autostart/fix_resolution.desktop
