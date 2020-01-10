52f54ea4d522082ed99fd20f37d1297545068496
Victor Reis <57648039+reis-victor@users.noreply.github.com>
2020-01-10 20:33:24 -0300

    [Update] Simplified code

--- cedilla_fix.sh
+++ cedilla_fix.sh
@@ -3,9 +3,6 @@
 # A simple script that selects the US-International keyboard layout and enables the cedilla using the ' + c keys combination
 # This script should be executed with root privileges
 
-# Loads US-International keyboard layout
-loadkeys us-acentos
-
-# Enables cedilha for GTK and QT applications
-echo 'GTK_IM_MODULE="cedilla"
-QT_IM_MODULE="cedilla"' >> /etc/environment
+# Enables cedilla for GTK and QT applications
+echo 'GTK_IM_MODULE=cedilla
+QT_IM_MODULE=cedilla' >> /etc/environment
