313592b87a0de746cc70eb79c612aa8f3ee35889
Victor Reis <57648039+reis-victor@users.noreply.github.com>
2020-01-13 01:42:10 -0300

    [Update] Improved script for the most popular screen resolutions
    Added prompt asking for the user resolution, providing options for 1080, 2k and 4k displays

---
+++ retina_resolution.sh
@@ -0,0 +1,51 @@
+#! /bin/bash
+
+# A Xorg/Gnome-oriented bash script that creates a virtual resolution in order to simulate a 'retina' display
+
+
+echo -e "Please, type your resolution (1)920x1080/(2)560x1440/(3)840x2160" 
+read RESOLUTION
+case "$RESOLUTION" in
+1)echo "Your resolution is Full HD: 1920x1080"
+  RESOLUTION=2560x1440
+;;
+2)echo "Your resolution is 2k: 2560x1440"
+  RESOLUTION=3840x2160
+;;
+3)echo "Your resolution is 4k: 3840x2160"
+  RESOLUTION=5760x3240
+;;
+esac
+
+
+# Creates the needed files: retina_resolution.sh and retina_resolution.desktop
+touch $HOME/retina_resolution.sh $HOME/.config/autostart/retina_resolution.desktop
+
+
+# Finds the primary output and assigns it to the connected_display variable
+DISPLAY=$(xrandr --verbose 2>/dev/null | grep connected | grep -v disconnected | cut -d' ' -f1)
+
+
+cat <<EOF > $HOME/retina_resolution.sh
+# 2x UI(User Interface) scaling
+gsettings set org.gnome.desktop.interface scaling-factor 2
+gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "[{'Gdk/WindowScalingFactor', <2>}]"
+# 4k virtual resolution scaled to 2k.
+xrandr --output $DISPLAY --panning $RESOLUTION --scale 1.5x1.5
+EOF
+
+
+# Gives execute permission to all users
+chmod +x $HOME/retina_resolution.sh
+
+
+# Creates an autostart script for the resolution adjustment
+cat <<EOF > $HOME/.config/autostart/retina_resolution.desktop
+[Desktop Entry]
+Name=retina_resolution
+Comment=Retina resolution
+Exec=$HOME/retina_resolution.sh
+Terminal=false
+Type=Application
+X-GNOME-Autostart-enabled=true
+EOF
