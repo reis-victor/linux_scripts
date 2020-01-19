82a602a7d39ddc3d2dfa732537785e9ce78e8ef3
Victor Reis <57648039+reis-victor@users.noreply.github.com>
2020-01-18 21:07:18 -0300

    [Update] Simplified Code

--- backup.sh
+++ backup.sh
@@ -4,22 +4,22 @@
 # This script should be run using sudo if you plan to backup system-wide folders 
 
 # Requests the backup source and its destination 
-read -p 'Please, select the backup source: ' SOURCE_VAR
-read -p 'Please, select the backup destination: ' DESTINATION_VAR
+read -p 'Please, select the backup source: ' SOURCE
+read -p 'Please, select the backup destination: ' DESTINATION
 echo
-echo The backup source is $SOURCE_VAR and its destination is $DESTINATION_VAR
+echo The backup source is $SOURCE and its destination is $DESTINATION
 echo
 
 # Checks if the user wants to remove destination files do not matching the source
-read -p 'Type YES to delete the destination folder files that do not match the source: ' DELETE_VAR
+read -p 'Type YES to delete the destination folder files that do not match the source: ' DELETE
 
-if [ "$DELETE_VAR" = "YES" ]
+if [ "$DELETE" = "YES" ]
 then
 # Copies files from the source to the destination and deletes the destination files that do not match the source
-rsync -aP --delete $SOURCE_VAR $DESTINATION_VAR
+rsync -aP --delete $SOURCE $DESTINATION
 else
 # Copies files from the source to the destination without removing files that do not match the source
-rsync -aP $SOURCE_VAR $DESTINATION_VAR
+rsync -aP $SOURCE $DESTINATION
 fi
 
 echo
