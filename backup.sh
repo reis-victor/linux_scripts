78adcee79077aad286c24d305de6f862e829cf16
Victor Reis <57648039+reis-victor@users.noreply.github.com>
2020-05-11 12:22:37 -0300

    [Update] Better use of shell function
    "grep" instead of the usual "if", since the latter one works as "test" instead of the usual "if" used in other languages

--- backup.sh
+++ backup.sh
@@ -4,23 +4,16 @@
 # This script should be run using sudo if you plan to backup system-wide folders 
 
 # Requests the backup source and its destination 
-read -p 'Please, select the backup source: ' SOURCE
-read -p 'Please, select the backup destination: ' DESTINATION
+read -p 'Please, select the backup source using the format /dev/sdx:   ' SOURCE
+echo
+read -p 'Please, select the backup destination using the format /dev/sdx:   ' DESTINATION
 echo
 echo The backup source is $SOURCE and its destination is $DESTINATION
 echo
 
 # Checks if the user wants to remove destination files do not matching the source
 read -p 'Type YES to delete the destination folder files that do not match the source: ' DELETE
-
-if [ "$DELETE" = "YES" ]
-then
-# Copies files from the source to the destination and deletes the destination files that do not match the source
-rsync -aP --delete $SOURCE $DESTINATION
-else
-# Copies files from the source to the destination without removing files that do not match the source
-rsync -aP $SOURCE $DESTINATION
-fi
-
+echo
+grep -q YES <<< $DELETE && rsync -aP --delete $SOURCE $DESTINATION || rsync -aP $SOURCE $DESTINATION
 echo
 echo Done
