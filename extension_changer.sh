3516d6ceafc3236916733fa7f3c9d1cfd1a2664d
Victor Reis <victorreis777@gmail.com>
2020-05-12 10:08:18 -0300

    [New] A simple extension changer script

---
+++ extension_changer.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+#This is a simple extension renamer script thant changes the old extension to a new one in a giver directory
+
+read -p "Please, type the extension to be renamed:  " OLD
+echo
+read -p "Please, type the new extension:    " NEW
+echo
+read -p "Please, type the desired directory:   " DIR
+
+for FILE in $DIR/*.$OLD
+do
+	mv "$FILE" "${FILE%.$OLD}.$NEW"
+done
