579e74cad3cad2fa5403e9e22c544374d653bb7c
Victor Reis <57648039+reis-victor@users.noreply.github.com>
2020-05-20 01:03:41 -0300

    [Update] Improved script
    The script creates the backup.sh script to be executed daily at 10 PM by cron

--- backup.sh
+++ backup.sh
@@ -1,12 +1,15 @@
 #! /bin/bash
 
+cat <<EOF > $HOME/Documents/scripts/backup.sh
+#! /bin/bash
+
 # A simple script that makes incremental backups using rsync
 # This script should be run using sudo if you plan to backup system-wide folders 
 
 # Requests the backup source and its destination 
-read -p 'Please, select the backup source using the format /dev/sdx:   ' SOURCE
+read -p 'Please, input the backup source:   ' SOURCE
 echo
-read -p 'Please, select the backup destination using the format /dev/sdx:   ' DESTINATION
+read -p 'Please, input the backup destination:   ' DESTINATION
 echo
 echo The backup source is $SOURCE and its destination is $DESTINATION
 echo
@@ -14,6 +17,15 @@ echo
 # Checks if the user wants to remove destination files do not matching the source
 read -p 'Type YES to delete the destination folder files that do not match the source: ' DELETE
 echo
-grep -q YES <<< $DELETE && rsync -aP --delete $SOURCE $DESTINATION || rsync -aP $SOURCE $DESTINATION
+grep -q YES <<< $DELETE && sudo rsync -aAXv --delete --checksum --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","swapfile","lost+found","ecryptfs","$HOME/.cache/*","$HOME/.mozilla/*"} $SOURCE $DESTINATION ||sudo rsync -aAXv --checksum --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","swapfile","lost+found","ecryptfs","$HOME/.cache/*","$HOME/.mozilla/*"} $SOURCE $DESTINATION 
 echo
 echo Done
+EOF
+
+SCRIPT_LOCATION=$HOME/Documents/scripts/backup.sh
+chmod +x $SCRIPT_LOCATION
+
+cat <<EOF > /etc/cron.daily/backup
+#! /bin/bash
+00 22 * * * $SCRIPT_LOCATION
+EOF
